8c8e7483ead8eb8347b3f4f5e98a1ca49d900600
Maxim Baz <git@maximbaz.com>
2019-12-01 12:13:48 +0100

    solve day1 part1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,14 @@
+use std::fs;
+
 pub fn part1(input: &str) {
-    println!("day1 part1");
+    let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
+    let total_fuel = contents
+        .lines()
+        .map(|l| l.parse::<f64>().unwrap())
+        .map(|f| (f / 3.0).floor() - 2.0)
+        .sum::<f64>();
+
+    println!("Day 1, Part 1: Total fuel = {}", total_fuel);
 }
 
 pub fn part2(input: &str) {
