f24c8f65078ac99e24e7eca17851f4650e819c59
Maxim Baz <git@maximbaz.com>
2019-12-05 00:38:10 +0100

    day3: implement using HashSet

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
+use std::hash::{Hash, Hasher};
 
 pub fn run_part1() -> i32 {
     let (line1, line2) = input();
@@ -11,7 +12,7 @@ pub fn run_part2() -> i32 {
     part2(line1, line2)
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 enum Segment {
     U(i32),
     D(i32),
@@ -20,12 +21,9 @@ enum Segment {
 }
 
 impl Segment {
-    fn n(&self) -> i32 {
+    fn size(&self) -> i32 {
         match *self {
-            Segment::U(n) => n,
-            Segment::D(n) => n,
-            Segment::L(n) => n,
-            Segment::R(n) => n,
+            Segment::U(n) | Segment::D(n) | Segment::L(n) | Segment::R(n) => n,
         }
     }
 
@@ -46,200 +44,152 @@ impl Segment {
     }
 }
 
-enum Square {
-    Line1(i32),
-    Line2,
-    Cross(i32),
+struct Point {
+    x: i32,
+    y: i32,
+    distance: i32,
 }
 
-type Line = Vec<Segment>;
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
 
-fn input() -> (Line, Line) {
+impl Eq for Point {}
+
+impl Hash for Point {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl Point {
+    fn manhattan(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+
+    fn sum_distances(&self, line1: &Line, line2: &Line) -> i32 {
+        line1.get(self).unwrap().distance + line2.get(self).unwrap().distance
+    }
+}
+
+type Segments = Vec<Segment>;
+type Line = HashSet<Point>;
+
+fn input() -> (Segments, Segments) {
     let lines: Vec<Vec<Segment>> = fs::read_to_string("data/day3.txt")
         .expect("Error reading the file")
         .trim()
         .lines()
-        .map(|line| {
-            line.split(",")
-                .map(|s| match s.chars().next().unwrap() {
-                    'U' => Segment::U(s[1..].parse().unwrap()),
-                    'D' => Segment::D(s[1..].parse().unwrap()),
-                    'L' => Segment::L(s[1..].parse().unwrap()),
-                    'R' => Segment::R(s[1..].parse().unwrap()),
-                    _ => unreachable!(),
-                })
-                .collect()
-        })
+        .map(parse_segments)
         .collect();
 
     (lines[0].clone(), lines[1].clone())
 }
 
-fn build_map(line1: Line, line2: Line) -> HashMap<(i32, i32), Square> {
-    let mut map = HashMap::new();
+fn parse_segments(string: &str) -> Segments {
+    string
+        .split(",")
+        .map(|s| match s.chars().next().unwrap() {
+            'U' => Segment::U(s[1..].parse().unwrap()),
+            'D' => Segment::D(s[1..].parse().unwrap()),
+            'L' => Segment::L(s[1..].parse().unwrap()),
+            'R' => Segment::R(s[1..].parse().unwrap()),
+            _ => unreachable!(),
+        })
+        .collect()
+}
 
+fn draw_line(segments: Segments) -> Line {
+    let mut points = HashSet::new();
     let mut x = 0;
     let mut y = 0;
-    let mut step = 0;
-    for segment in line1 {
-        for _i in 0..segment.n() {
+    let mut distance = 0;
+    for segment in segments {
+        for _i in 0..segment.size() {
             x += segment.x_offset();
             y += segment.y_offset();
-            step += 1;
-            map.insert((x, y), Square::Line1(step));
+            distance += 1;
+            points.insert(Point { x, y, distance });
         }
     }
 
-    x = 0;
-    y = 0;
-    step = 0;
-
-    for segment in line2 {
-        for _i in 0..segment.n() {
-            x += segment.x_offset();
-            y += segment.y_offset();
-            step += 1;
-            map.insert(
-                (x, y),
-                match map.get(&(x, y)) {
-                    Some(Square::Line1(n)) => Square::Cross(n + step),
-                    _ => Square::Line2,
-                },
-            );
-        }
-    }
-
-    map
+    points
 }
 
-fn part1(line1: Line, line2: Line) -> i32 {
-    let map = build_map(line1, line2);
-
-    let mut min_dist = i32::max_value();
-
-    for ((i, j), segment) in map {
-        if let Square::Cross(_) = segment {
-            let dist = i.abs() + j.abs();
-            if dist < min_dist {
-                min_dist = dist;
-            }
-        }
-    }
-
-    min_dist
+fn part1(segments1: Segments, segments2: Segments) -> i32 {
+    draw_line(segments1)
+        .intersection(&draw_line(segments2))
+        .min_by_key(|p| p.manhattan())
+        .unwrap()
+        .manhattan()
 }
 
-fn part2(line1: Line, line2: Line) -> i32 {
-    let map = build_map(line1, line2);
+fn part2(segments1: Segments, segments2: Segments) -> i32 {
+    let line1 = draw_line(segments1);
+    let line2 = draw_line(segments2);
 
-    let mut min_dist = i32::max_value();
-
-    for (_, segment) in map {
-        if let Square::Cross(dist) = segment {
-            if dist < min_dist {
-                min_dist = dist;
-            }
-        }
-    }
-
-    min_dist
+    line1
+        .intersection(&line2)
+        .min_by_key(|p| p.sum_distances(&line1, &line2))
+        .unwrap()
+        .sum_distances(&line1, &line2)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::parse_segments as p;
     use super::Segment::*;
     use super::*;
 
     #[test]
-    fn test_part1() {
+    fn test_parse_segments() {
         assert_eq!(
-            6,
-            part1(vec![R(8), U(5), L(5), D(3)], vec![U(7), R(6), D(4), L(4)])
+            vec![R(75), D(30), U(83), L(12), D(49), R(71), U(7), L(72)],
+            parse_segments("R75,D30,U83,L12,D49,R71,U7,L72")
         );
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(6, part1(p("R8,U5,L5,D3"), p("U7,R6,D4,L4")));
 
         assert_eq!(
             159,
             part1(
-                vec![R(75), D(30), R(83), U(83), L(12), D(49), R(71), U(7), L(72)],
-                vec![U(62), R(66), U(55), R(34), D(71), R(55), D(58), R(83)]
+                p("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+                p("U62,R66,U55,R34,D71,R55,D58,R83")
             )
         );
 
         assert_eq!(
             135,
             part1(
-                vec![
-                    R(98),
-                    U(47),
-                    R(26),
-                    D(63),
-                    R(33),
-                    U(87),
-                    L(62),
-                    D(20),
-                    R(33),
-                    U(53),
-                    R(51)
-                ],
-                vec![
-                    U(98),
-                    R(91),
-                    D(20),
-                    R(16),
-                    D(67),
-                    R(40),
-                    U(7),
-                    R(15),
-                    U(6),
-                    R(7)
-                ]
+                p("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+                p("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
             )
         );
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(
-            30,
-            part2(vec![R(8), U(5), L(5), D(3)], vec![U(7), R(6), D(4), L(4)])
-        );
+        assert_eq!(30, part2(p("R8,U5,L5,D3"), p("U7,R6,D4,L4")));
 
         assert_eq!(
             610,
             part2(
-                vec![R(75), D(30), R(83), U(83), L(12), D(49), R(71), U(7), L(72)],
-                vec![U(62), R(66), U(55), R(34), D(71), R(55), D(58), R(83)]
+                p("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+                p("U62,R66,U55,R34,D71,R55,D58,R83")
             )
         );
 
         assert_eq!(
             410,
             part2(
-                vec![
-                    R(98),
-                    U(47),
-                    R(26),
-                    D(63),
-                    R(33),
-                    U(87),
-                    L(62),
-                    D(20),
-                    R(33),
-                    U(53),
-                    R(51)
-                ],
-                vec![
-                    U(98),
-                    R(91),
-                    D(20),
-                    R(16),
-                    D(67),
-                    R(40),
-                    U(7),
-                    R(15),
-                    U(6),
-                    R(7)
-                ]
+                p("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+                p("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
             )
         );
     }
