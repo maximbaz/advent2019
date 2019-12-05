7467cd3fbfb49bbefba5a352c84ef14ecb401c89
Maxim Baz <git@maximbaz.com>
2019-12-05 22:44:52 +0100

    day5: refactor, add tests

--- src/day5.rs
+++ src/day5.rs
@@ -25,144 +25,169 @@ fn part2(input: Vec<i32>) -> i32 {
     solve(input, 5)
 }
 
-fn parse_opcode(mut n: i32) -> Vec<i8> {
-    let mut digits = Vec::new();
-    while n > 9 {
-        digits.push((n % 10) as i8);
-        n = n / 10;
-    }
-    digits.push(n as i8);
-    for _ in 0..5 {
-        digits.push(0);
-    }
-    digits
-}
-
-fn solve(mut memory: Vec<i32>, input: i32) -> i32 {
+fn solve(mut mem: Vec<i32>, input: i32) -> i32 {
     let mut pos = 0;
+    let mut out = 0;
 
-    let mut output = -1;
-    while memory[pos] != 99 {
-        let opcode = parse_opcode(memory[pos]);
-        match opcode[0] {
-            1 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                let dst = memory[pos + 3] as usize;
-                memory[dst] = a + b;
-                pos += 4;
+    loop {
+        match execute(mem, input, pos, out) {
+            Ok((mem_new, pos_new, out_new)) => {
+                mem = mem_new;
+                pos = pos_new;
+                out = out_new;
             }
-            2 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                let dst = memory[pos + 3] as usize;
-                memory[dst] = a * b;
-                pos += 4;
-            }
-            3 => {
-                let dst = memory[pos + 1] as usize;
-                memory[dst] = input;
-                pos += 2;
-            }
-            4 => {
-                let dst = memory[pos + 1] as usize;
-                output = memory[dst];
-                pos += 2;
-            }
-            5 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                if a != 0 {
-                    pos = b as usize;
-                } else {
-                    pos += 3;
-                }
-            }
-            6 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                if a == 0 {
-                    pos = b as usize;
-                } else {
-                    pos += 3;
-                }
-            }
-            7 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                let dst = memory[pos + 3] as usize;
-                memory[dst] = if a < b { 1 } else { 0 };
-                pos += 4;
-            }
-            8 => {
-                let a = memory[pos + 1];
-                let a = if opcode[2] == 0 {
-                    memory[a as usize]
-                } else {
-                    a
-                };
-                let b = memory[pos + 2];
-                let b = if opcode[3] == 0 {
-                    memory[b as usize]
-                } else {
-                    b
-                };
-                let dst = memory[pos + 3] as usize;
-                memory[dst] = if a == b { 1 } else { 0 };
-                pos += 4;
-            }
-            _ => unreachable!(),
+            Err(_) => return out,
         }
     }
-    output
+}
+
+fn execute(
+    memr: Vec<i32>,
+    input: i32,
+    pos: usize,
+    output: i32,
+) -> Result<(Vec<i32>, usize, i32), ()> {
+    let mut memw = memr.clone();
+
+    let is_pos_mode = |i: usize| (memr[pos] / 10i32.pow(i as u32 + 1)) % 10 == 0;
+
+    let read = |i: usize| {
+        if is_pos_mode(i) {
+            memr[memr[pos + i] as usize]
+        } else {
+            memr[pos + i]
+        }
+    };
+
+    let mut write = |i: usize, val: i32| {
+        memw[memr[pos + i] as usize] = val;
+    };
+
+    match memr[pos] % 100 {
+        99 => Err(()),
+        1 => {
+            write(3, read(1) + read(2));
+            Ok((memw, pos + 4, output))
+        }
+        2 => {
+            write(3, read(1) * read(2));
+            Ok((memw, pos + 4, output))
+        }
+        3 => {
+            write(1, input);
+            Ok((memw, pos + 2, output))
+        }
+        4 => Ok((memw, pos + 2, read(1))),
+        5 => {
+            if read(1) != 0 {
+                Ok((memw, read(2) as usize, output))
+            } else {
+                Ok((memw, pos + 3, output))
+            }
+        }
+        6 => {
+            if read(1) == 0 {
+                Ok((memw, read(2) as usize, output))
+            } else {
+                Ok((memw, pos + 3, output))
+            }
+        }
+        7 => {
+            write(3, if read(1) < read(2) { 1 } else { 0 });
+            Ok((memw, pos + 4, output))
+        }
+        8 => {
+            write(3, if read(1) == read(2) { 1 } else { 0 });
+            Ok((memw, pos + 4, output))
+        }
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_equal_less() {
+        assert_eq!(0, solve(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 7));
+        assert_eq!(1, solve(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8));
+        assert_eq!(0, solve(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 9));
+
+        assert_eq!(1, solve(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 7));
+        assert_eq!(0, solve(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 8));
+        assert_eq!(0, solve(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 9));
+
+        assert_eq!(0, solve(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 7));
+        assert_eq!(1, solve(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 8));
+        assert_eq!(0, solve(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 9));
+
+        assert_eq!(1, solve(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 7));
+        assert_eq!(0, solve(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 8));
+        assert_eq!(0, solve(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 9));
+    }
+
+    #[test]
+    fn test_solve_jump() {
+        assert_eq!(
+            0,
+            solve(
+                vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
+                0
+            )
+        );
+        assert_eq!(
+            1,
+            solve(
+                vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
+                9
+            )
+        );
+
+        assert_eq!(
+            0,
+            solve(vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 0)
+        );
+        assert_eq!(
+            1,
+            solve(vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 9)
+        );
+    }
+
+    #[test]
+    fn test_solve_large() {
+        assert_eq!(
+            999,
+            solve(
+                vec![
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0,
+                    36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46,
+                    1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ],
+                7
+            )
+        );
+        assert_eq!(
+            1000,
+            solve(
+                vec![
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0,
+                    36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46,
+                    1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ],
+                8
+            )
+        );
+        assert_eq!(
+            1001,
+            solve(
+                vec![
+                    3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0,
+                    36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46,
+                    1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+                ],
+                9
+            )
+        );
+    }
 }
