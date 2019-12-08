f50195fe06af6998b395ecd1133036bff2622a9d
Maxim Baz <git@maximbaz.com>
2019-12-08 19:31:58 +0100

    day6: rewrite using string slices and lifetimes

--- src/day6.rs
+++ src/day6.rs
@@ -3,36 +3,35 @@ use std::collections::HashSet;
 use std::fs;
 
 pub fn run_part1() -> u32 {
-    part1(input())
+    part1(input(&read_file()))
 }
 
 pub fn run_part2() -> u32 {
-    part2(input())
+    part2(input(&read_file()))
 }
 
-type Input = Vec<(String, String)>;
-type Graph = HashMap<String, Vec<String>>;
+type Pair<'a> = (&'a str, &'a str);
+type Input<'a> = Vec<Pair<'a>>;
+type Graph<'a, 'b> = HashMap<&'a str, Vec<&'b str>>;
 
-fn input() -> Input {
-    fs::read_to_string("data/day6.txt")
-        .expect("Error reading the file")
-        .trim()
-        .lines()
-        .map(split)
-        .collect()
+fn read_file() -> String {
+    fs::read_to_string("data/day6.txt").expect("Error reading the file")
 }
 
-fn split(s: &str) -> (String, String) {
+fn input<'a>(data: &'a String) -> Input<'a> {
+    data.trim().lines().map(split).collect()
+}
+
+fn split(s: &str) -> Pair {
     let parts = s.split(")").collect::<Vec<_>>();
-    (parts[0].to_owned(), parts[1].to_owned())
+    (parts[0], parts[1])
 }
 
 fn build_graph(input: Input, bi_directional: bool) -> Graph {
     let mut graph = HashMap::new();
 
     for line in input {
-        let pair = line.clone();
-        graph.entry(pair.0).or_insert(Vec::new()).push(pair.1);
+        graph.entry(line.0).or_insert(Vec::new()).push(line.1);
 
         if bi_directional {
             graph.entry(line.1).or_insert(Vec::new()).push(line.0);
@@ -44,33 +43,38 @@ fn build_graph(input: Input, bi_directional: bool) -> Graph {
 
 fn part1(input: Input) -> u32 {
     let graph = build_graph(input, false);
-    part1_search(&graph, "COM".to_owned(), 0)
+    part1_search(&graph, "COM", 0)
 }
 
 fn part2(input: Input) -> u32 {
     let graph = build_graph(input, true);
-    part2_search(&graph, "YOU".to_owned(), &mut HashSet::new(), 0) - 1
+    part2_search(&graph, "YOU", &mut HashSet::new(), 0) - 1
 }
 
-fn part1_search(graph: &Graph, item: String, dist: u32) -> u32 {
+fn part1_search(graph: &Graph, item: &str, dist: u32) -> u32 {
     let mut sum = dist;
 
     if let Some(children) = graph.get(&item) {
         for v in children {
-            sum += part1_search(graph, v.clone(), dist + 1);
+            sum += part1_search(graph, v, dist + 1);
         }
     }
 
     sum
 }
 
-fn part2_search(graph: &Graph, item: String, visited: &mut HashSet<String>, dist: u32) -> u32 {
+fn part2_search<'a>(
+    graph: &'a Graph,
+    item: &'a str,
+    visited: &mut HashSet<&'a str>,
+    dist: u32,
+) -> u32 {
     let mut santa_dist = 0;
-    visited.insert(item.clone());
+    visited.insert(item);
 
     if let Some(children) = graph.get(&item) {
         for v in children {
-            if v == "SAN" {
+            if v == &"SAN" {
                 return dist;
             }
             if !visited.contains(v) {
