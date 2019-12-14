eea3322216f5ee58e22087558aee6e1243143817
Maxim Baz <git@maximbaz.com>
2019-12-14 02:15:45 +0100

    day5: use intcode module

--- src/day5.rs
+++ src/day5.rs
@@ -1,14 +1,16 @@
 use std::fs;
 
-pub fn run_part1() -> i32 {
+use super::intcode::IntCode;
+
+pub fn run_part1() -> i64 {
     part1(input())
 }
 
-pub fn run_part2() -> i32 {
+pub fn run_part2() -> i64 {
     part2(input())
 }
 
-fn input() -> Vec<i32> {
+fn input() -> Vec<i64> {
     fs::read_to_string("data/day5.txt")
         .expect("Error reading the file")
         .trim()
@@ -17,91 +19,20 @@ fn input() -> Vec<i32> {
         .collect()
 }
 
-fn part1(input: Vec<i32>) -> i32 {
+fn part1(input: Vec<i64>) -> i64 {
     solve(input, 1)
 }
 
-fn part2(input: Vec<i32>) -> i32 {
+fn part2(input: Vec<i64>) -> i64 {
     solve(input, 5)
 }
 
-fn solve(mut mem: Vec<i32>, input: i32) -> i32 {
-    let mut pos = 0;
-    let mut out = 0;
+fn solve(mem: Vec<i64>, input: i64) -> i64 {
+    let mut result = 0;
 
-    loop {
-        match execute(mem, input, pos, out) {
-            Ok((mem_new, pos_new, out_new)) => {
-                mem = mem_new;
-                pos = pos_new;
-                out = out_new;
-            }
-            Err(_) => return out,
-        }
-    }
-}
+    IntCode::new(&mem).run(|| input, |output| result = output);
 
-fn execute(
-    memr: Vec<i32>,
-    input: i32,
-    pos: usize,
-    output: i32,
-) -> Result<(Vec<i32>, usize, i32), ()> {
-    let mut memw = memr.clone();
-
-    let is_pos_mode = |i: usize| (memr[pos] / 10i32.pow(i as u32 + 1)) % 10 == 0;
-
-    let read = |i: usize| {
-        if is_pos_mode(i) {
-            memr[memr[pos + i] as usize]
-        } else {
-            memr[pos + i]
-        }
-    };
-
-    let mut write = |i: usize, val: i32| {
-        memw[memr[pos + i] as usize] = val;
-    };
-
-    match memr[pos] % 100 {
-        99 => Err(()),
-        1 => {
-            write(3, read(1) + read(2));
-            Ok((memw, pos + 4, output))
-        }
-        2 => {
-            write(3, read(1) * read(2));
-            Ok((memw, pos + 4, output))
-        }
-        3 => {
-            write(1, input);
-            Ok((memw, pos + 2, output))
-        }
-        4 => Ok((memw, pos + 2, read(1))),
-        5 => {
-            if read(1) != 0 {
-                Ok((memw, read(2) as usize, output))
-            } else {
-                Ok((memw, pos + 3, output))
-            }
-        }
-        6 => {
-            if read(1) == 0 {
-                Ok((memw, read(2) as usize, output))
-            } else {
-                Ok((memw, pos + 3, output))
-            }
-        }
-        7 => {
-            write(3, if read(1) < read(2) { 1 } else { 0 });
-            Ok((memw, pos + 4, output))
-        }
-        8 => {
-            write(3, if read(1) == read(2) { 1 } else { 0 });
-            Ok((memw, pos + 4, output))
-        }
-        _ => unreachable!(),
-    }
+    result
 }
 
 #[cfg(test)]
