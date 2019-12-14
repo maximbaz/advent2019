6468e90e3609aa01925598a7f5a2241a74d73e7b
Maxim Baz <git@maximbaz.com>
2019-12-14 13:56:44 +0100

    day7: use intcode module

--- src/day7.rs
+++ src/day7.rs
@@ -2,18 +2,20 @@ use itertools::Itertools;
 use std::fs;
 
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::Sender;
 use std::thread;
 
-pub fn run_part1() -> i32 {
+use super::intcode::IntCode;
+
+pub fn run_part1() -> i64 {
     part1(input())
 }
 
-pub fn run_part2() -> i32 {
+pub fn run_part2() -> i64 {
     part2(input())
 }
 
-fn input() -> Vec<i32> {
+fn input() -> Vec<i64> {
     fs::read_to_string("data/day7.txt")
         .expect("Error reading the file")
         .trim()
@@ -22,25 +24,18 @@ fn input() -> Vec<i32> {
         .collect()
 }
 
-fn part1(input: Vec<i32>) -> i32 {
+fn part1(input: Vec<i64>) -> i64 {
     solve(input, false)
 }
 
-fn part2(input: Vec<i32>) -> i32 {
+fn part2(input: Vec<i64>) -> i64 {
     solve(input, true)
 }
 
-fn solve(memory: Vec<i32>, feedback_loop: bool) -> i32 {
-    let forward = |from: Receiver<i32>, to: Sender<i32>| loop {
-        if let Ok(msg) = from.recv() {
-            if let Err(_) = to.send(msg) {
-                return;
-            }
-        } else {
-            return;
-        }
+fn solve(memory: Vec<i64>, feedback_loop: bool) -> i64 {
+    let send = |chan: &Sender<i64>, val: i64| {
+        chan.send(val).unwrap_or(());
     };
-
     if feedback_loop { (5..10) } else { (0..5) }
         .permutations(5)
         .map(|perm| {
@@ -49,23 +44,11 @@ fn solve(memory: Vec<i32>, feedback_loop: bool) -> i32 {
             let (tx_input3, rx_input3) = mpsc::channel();
             let (tx_input4, rx_input4) = mpsc::channel();
             let (tx_input5, rx_input5) = mpsc::channel();
-            let (tx_output1, rx_output1) = mpsc::channel();
-            let (tx_output2, rx_output2) = mpsc::channel();
-            let (tx_output3, rx_output3) = mpsc::channel();
-            let (tx_output4, rx_output4) = mpsc::channel();
-            let (tx_output5, rx_output5) = mpsc::channel();
-            let (tx_final, rx_final) = mpsc::channel();
             let memory1 = memory.clone();
             let memory2 = memory.clone();
             let memory3 = memory.clone();
             let memory4 = memory.clone();
             let memory5 = memory.clone();
-            thread::spawn(move || run(memory1, rx_input1, tx_output1, None));
-            thread::spawn(move || run(memory2, rx_input2, tx_output2, None));
-            thread::spawn(move || run(memory3, rx_input3, tx_output3, None));
-            thread::spawn(move || run(memory4, rx_input4, tx_output4, None));
-            thread::spawn(move || run(memory5, rx_input5, tx_output5, Some(tx_final)));
-
             tx_input1.send(perm[0]).unwrap();
             tx_input2.send(perm[1]).unwrap();
             tx_input3.send(perm[2]).unwrap();
@@ -73,113 +56,48 @@ fn solve(memory: Vec<i32>, feedback_loop: bool) -> i32 {
             tx_input5.send(perm[4]).unwrap();
             tx_input1.send(0).unwrap();
 
-            thread::spawn(move || forward(rx_output1, tx_input2));
-            thread::spawn(move || forward(rx_output2, tx_input3));
-            thread::spawn(move || forward(rx_output3, tx_input4));
-            thread::spawn(move || forward(rx_output4, tx_input5));
-            if feedback_loop {
-                thread::spawn(move || forward(rx_output5, tx_input1));
-            }
-
-            rx_final.recv().unwrap()
+            thread::spawn(move || {
+                IntCode::new(&memory1).run(
+                    || rx_input1.recv().unwrap(),
+                    |output| send(&tx_input2, output),
+                );
+            });
+            thread::spawn(move || {
+                IntCode::new(&memory2).run(
+                    || rx_input2.recv().unwrap(),
+                    |output| send(&tx_input3, output),
+                );
+            });
+            thread::spawn(move || {
+                IntCode::new(&memory3).run(
+                    || rx_input3.recv().unwrap(),
+                    |output| send(&tx_input4, output),
+                );
+            });
+            thread::spawn(move || {
+                IntCode::new(&memory4).run(
+                    || rx_input4.recv().unwrap(),
+                    |output| send(&tx_input5, output),
+                );
+            });
+            thread::spawn(move || {
+                let mut result = 0;
+                IntCode::new(&memory5).run(
+                    || rx_input5.recv().unwrap(),
+                    |output| {
+                        result = output;
+                        if feedback_loop {
+                            send(&tx_input1, output);
+                        }
+                    },
+                );
+                result
+            })
+            .join()
+            .unwrap()
         })
         .max()
         .unwrap()
-}
-
-enum Result {
-    Done,
-    OutputProduced(Vec<i32>, usize, i32),
-    OtherOk(Vec<i32>, usize),
-}
-
-fn run(
-    mut mem: Vec<i32>,
-    rx_input: Receiver<i32>,
-    tx_output: Sender<i32>,
-    tx_last_output: Option<Sender<i32>>,
-) -> i32 {
-    let mut pos = 0;
-    let mut last_output = 0;
-
-    loop {
-        match execute(mem, pos, &rx_input) {
-            Result::Done => {
-                if let Some(chan) = tx_last_output {
-                    chan.send(last_output).unwrap();
-                }
-                return last_output;
-            }
-            Result::OutputProduced(new_mem, new_pos, new_output) => {
-                mem = new_mem;
-                pos = new_pos;
-                last_output = new_output;
-                tx_output.send(new_output).unwrap();
-            }
-            Result::OtherOk(new_mem, new_pos) => {
-                mem = new_mem;
-                pos = new_pos;
-            }
-        }
-    }
-}
-
-fn execute(memr: Vec<i32>, pos: usize, rx_input: &Receiver<i32>) -> Result {
-    let mut memw = memr.clone();
-
-    let is_pos_mode = |i: usize| (memr[pos] / 10i32.pow(i as u32 + 1)) % 10 == 0;
-
-    let read = |i: usize| {
-        if is_pos_mode(i) {
-            memr[memr[pos + i] as usize]
-        } else {
-            memr[pos + i]
-        }
-    };
-
-    let mut write = |i: usize, val: i32| {
-        memw[memr[pos + i] as usize] = val;
-    };
-
-    match memr[pos] % 100 {
-        99 => Result::Done,
-        1 => {
-            write(3, read(1) + read(2));
-            Result::OtherOk(memw, pos + 4)
-        }
-        2 => {
-            write(3, read(1) * read(2));
-            Result::OtherOk(memw, pos + 4)
-        }
-        3 => {
-            write(1, rx_input.recv().unwrap());
-            Result::OtherOk(memw, pos + 2)
-        }
-        4 => Result::OutputProduced(memw, pos + 2, read(1)),
-        5 => {
-            if read(1) != 0 {
-                Result::OtherOk(memw, read(2) as usize)
-            } else {
-                Result::OtherOk(memw, pos + 3)
-            }
-        }
-        6 => {
-            if read(1) == 0 {
-                Result::OtherOk(memw, read(2) as usize)
-            } else {
-                Result::OtherOk(memw, pos + 3)
-            }
-        }
-        7 => {
-            write(3, if read(1) < read(2) { 1 } else { 0 });
-            Result::OtherOk(memw, pos + 4)
-        }
-        8 => {
-            write(3, if read(1) == read(2) { 1 } else { 0 });
-            Result::OtherOk(memw, pos + 4)
-        }
-        _ => unreachable!(),
-    }
 }
 
 #[cfg(test)]
