af7673295a2a724f6530b88f4386c61e2db45ede
Maxim Baz <git@maximbaz.com>
2019-12-14 02:19:30 +0100

    day9: use intcode module

--- src/day9.rs
+++ src/day9.rs
@@ -1,8 +1,6 @@
 use std::fs;
 
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
-use std::thread;
+use super::intcode::IntCode;
 
 pub fn run_part1() -> i64 {
     part1(input())
@@ -29,133 +27,12 @@ fn part2(input: Vec<i64>) -> i64 {
     solve(input, 2)
 }
 
-fn solve(mut memory: Vec<i64>, input: i64) -> i64 {
-    // cheat by adding some buffer to the vector
-    for _ in 0..100000 {
-        memory.push(0);
-    }
+fn solve(memory: Vec<i64>, input: i64) -> i64 {
+    let mut result = 0;
 
-    let (tx_input, rx_input) = mpsc::channel();
-    let (tx_final, rx_final) = mpsc::channel();
-    thread::spawn(move || run(memory, &rx_input, None, Some(&tx_final)));
+    IntCode::new(&memory).run(|| input, |output| result = output);
 
-    tx_input.send(input).unwrap();
-
-    rx_final.recv().unwrap()
-}
-
-enum Result {
-    Done,
-    OutputProduced(Vec<i64>, usize, i64),
-    RelPosChanged(Vec<i64>, usize, usize),
-    OtherOk(Vec<i64>, usize),
-}
-
-fn run(
-    mut mem: Vec<i64>,
-    rx_input: &Receiver<i64>,
-    tx_output: Option<&Sender<i64>>,
-    tx_last_output: Option<&Sender<i64>>,
-) -> i64 {
-    let mut pos = 0;
-    let mut rel_pos = 0;
-    let mut last_output = 0;
-
-    loop {
-        match execute(mem, pos, rel_pos, rx_input) {
-            Result::Done => {
-                if let Some(last_output_channel) = tx_last_output {
-                    last_output_channel.send(last_output).unwrap();
-                }
-                return last_output;
-            }
-            Result::OutputProduced(new_mem, new_pos, new_output) => {
-                mem = new_mem;
-                pos = new_pos;
-                last_output = new_output;
-                if let Some(output_channel) = tx_output {
-                    output_channel.send(new_output).unwrap();
-                }
-            }
-            Result::RelPosChanged(new_mem, new_pos, new_rel_pos) => {
-                mem = new_mem;
-                pos = new_pos;
-                rel_pos = new_rel_pos;
-            }
-            Result::OtherOk(new_mem, new_pos) => {
-                mem = new_mem;
-                pos = new_pos;
-            }
-        }
-    }
-}
-
-fn execute(memr: Vec<i64>, pos: usize, rel_pos: usize, rx_input: &Receiver<i64>) -> Result {
-    let mut memw = memr.clone();
-
-    let is_pos_mode = |i: usize| (*memr.get(pos).unwrap_or(&0) / 10i64.pow(i as u32 + 1)) % 10 == 0;
-    let is_rel_mode = |i: usize| (*memr.get(pos).unwrap_or(&0) / 10i64.pow(i as u32 + 1)) % 10 == 2;
-
-    let read = |i: usize| {
-        if is_pos_mode(i) {
-            memr[memr[pos + i] as usize]
-        } else if is_rel_mode(i) {
-            memr[(rel_pos as i64 + memr[pos + i]) as usize]
-        } else {
-            memr[pos + i]
-        }
-    };
-
-    let mut write = |i: usize, val: i64| {
-        if is_pos_mode(i) {
-            memw[memr[pos + i] as usize] = val;
-        } else if is_rel_mode(i) {
-            memw[(rel_pos as i64 + memr[pos + i]) as usize] = val;
-        } else {
-            panic!("write can never occur in immediate mode!");
-        }
-    };
-
-    match memr[pos] % 100 {
-        99 => Result::Done,
-        1 => {
-            write(3, read(1) + read(2));
-            Result::OtherOk(memw, pos + 4)
-        }
-        2 => {
-            write(3, read(1) * read(2));
-            Result::OtherOk(memw, pos + 4)
-        }
-        3 => {
-            write(1, rx_input.recv().unwrap());
-            Result::OtherOk(memw, pos + 2)
-        }
-        4 => Result::OutputProduced(memw, pos + 2, read(1)),
-        5 => {
-            if read(1) != 0 {
-                Result::OtherOk(memw, read(2) as usize)
-            } else {
-                Result::OtherOk(memw, pos + 3)
-            }
-        }
-        6 => {
-            if read(1) == 0 {
-                Result::OtherOk(memw, read(2) as usize)
-            } else {
-                Result::OtherOk(memw, pos + 3)
-            }
-        }
-        7 => {
-            write(3, if read(1) < read(2) { 1 } else { 0 });
-            Result::OtherOk(memw, pos + 4)
-        }
-        8 => {
-            write(3, if read(1) == read(2) { 1 } else { 0 });
-            Result::OtherOk(memw, pos + 4)
-        }
-        9 => Result::RelPosChanged(memw, pos + 2, (rel_pos as i64 + read(1)) as usize),
-        _ => unreachable!(),
-    }
+    result
 }
 
 #[cfg(test)]
